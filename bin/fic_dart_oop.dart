274cc49a89a8c1f6b450e6caf0b73e345f0f126c
irfanzidniofficial <irfanzidniofficial@gmail.com>
2023-03-19 22:18:45 +0700

    first commit

---
+++ bin/fic_dart_oop.dart
@@ -0,0 +1,5 @@
+import 'package:fic_dart_oop/fic_dart_oop.dart' as fic_dart_oop;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fic_dart_oop.calculate()}!');
+}
---
+++ test/fic_dart_oop_test.dart
@@ -0,0 +1,8 @@
+import 'package:fic_dart_oop/fic_dart_oop.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
